5708b7821a0ef4db7991744fe571e112bd058576
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2026-01-18 01:11:15 +0100

    feat: Pull automatique front
    Récupération automatique du front-end lors d'une publication d'une release sur le GitHub et mise en ligne.

--- README.md
+++ README.md
@@ -11,10 +11,11 @@ cd back_clinique_val_lumineux
 npm install
 ```
 
-Après cela, il faut créer un fichier `.env` qui doit contenir les informations suivantes :
+Après cela, il faut créer un fichier `.env` qui doit contenir au moins les informations suivantes :
 - `SERVER_IP` : IP sur lequel le serveur Web tournera
 - `SERVER_PORT` : Port sur lequel le serveur Web écoutera
 - `SITE_FOLDER` : Dossier dans lequel se trouvera le site web
+
 
 Exemple de `.env`:
 ```env
@@ -24,6 +25,11 @@ SITE_FOLDER=./site
 ```
 
 Après cela, le serveur est installé.
+
+Il est possible d'utiliser les WebHooks GitHub pour importer le front du site lorsqu'une release est faite.
+L'adresse du WebHook est `http://votre.site/github`. Afin d'éviter l'usurpation lorsque vous recevez un WebHook, vous pouvez utiliser une [clé secrète sur le WebHook](https://docs.github.com/en/webhooks/using-webhooks/validating-webhook-deliveries).
+
+Pour utiliser cette clef secrète de votre côté aussi, il suffit d'ajouter la variable d'environnement `GITHUB_FRONT_SECRET` et d'y associé votre clé.
 
 ### Lancement
 ---
--- githubRequest.js
+++ githubRequest.js
@@ -1,7 +1,13 @@
+const { createHash, timingSafeEqual, createHmac } = require('crypto');
+const fs = require('fs');
+const decompress = require("decompress");
+
 const env = process.env;
 
+const SITE_FOLDER = env.SITE_FOLDER;
+
 const minimalGitRequestHeaders = [
-    "HTTP_USER_AGENT", "HTTP_X_GITHUB_HOOK_ID", "HTTP_X_GITHUB_EVENT", "HTTP_X_GITHUB_DELIVERY", "HTTP_X_GITHUB_HOOK_INSTALLATION_TARGET_TYPE"
+    "HTTP_USER_AGENT", "HTTP_X_GITHUB_HOOK_ID", "HTTP_X_GITHUB_EVENT", "HTTP_X_GITHUB_DELIVERY", "HTTP_X_GITHUB_HOOK_INSTALLATION_TARGET_TYPE",
 ];
 
 async function handleRequest(req) { // headers: dictionnaire ; contentString: string
@@ -15,23 +21,52 @@ async function handleRequest(req) { // headers: dictionnaire ; contentString: st
         i++;
     }
 
+    var contentString;
     var contentJSON;
     
     try {
-        contentJSON = JSON.parse(await getData(req));
+        contentString = await getData(req);
+        contentJSON = JSON.parse(contentString);
     } catch (error) {}
 
     if(allHeaders &&
         headers["HTTP_USER_AGENT"].startsWith("GitHub-Hookshot/") &&
         headers["HTTP_X_GITHUB_HOOK_INSTALLATION_TARGET_TYPE"] == "repository" &&
+        headers["HTTP_X_GITHUB_EVENT"] == "release" &&
         contentJSON != undefined
-    ) {
-        // TODO
-        console.log(contentJSON)
+    ) { // mise à jour Front
+        var validSender = true;
+
+        if(env.GITHUB_FRONT_SECRET != undefined) {
+            validSender = timingSafeEqual( // vérification via HMAC-SHA256 de l'authenticité du WebHook
+                headers["X-HUB-SIGNATURE-256"].replace("sha256=", ""),
+                createHmac('sha256', env.GITHUB_FRONT_SECRET).update(contentString).digest("hex")
+            );
+        }
+
+        if(validSender) {
+            const downloadURL = contentJSON["release"]["assets"][0]["browser_download_url"];
+            const ZIP = SITE_FOLDER + ".zip";
+            await downloadFile(downloadURL, ZIP);
+            if(fs.existsSync(SITE_FOLDER)) {
+                fs.rmSync(SITE_FOLDER, {recursive: true, force: true});
+            }
+            decompress(ZIP, SITE_FOLDER).then(() => {
+                fs.rmSync(ZIP);
+            }).catch((error) => console.log(error));
+        }
     } else {
         res = { statusCode: 302, location: '/404' };
     }
     return res;
+}
+
+async function downloadFile(from, to) {
+    try {
+        await fetch(from).then(async (r) => {
+            fs.writeFileSync(to, Buffer.from(await r.arrayBuffer()));
+        });
+    }catch(error) { }
 }
 
 async function getData(req) {
