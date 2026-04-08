bde4c2e37c99efdffb33697f75242449c5914961
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2026-04-08 14:10:48 +0200

    feat: possibilité de lier un client à un utilisateur

--- api/clients.js
+++ api/clients.js
@@ -3,6 +3,11 @@ async function handle(method, splittedRoute, headers, data, queryParameters, que
     switch(method) {
         case "GET":
             res = await handleGet(splittedRoute, headers, data, query);
+            break;
+        case "PUT":
+            res = await handlePut(splittedRoute, headers, data, query);
+            break;
+
     }
     return res;
 }
@@ -37,6 +42,57 @@ async function getClients(headers, query) {
             ON U.id=UT.user \
             WHERE UT.token="${token}"
         `);
+    }
+
+    return res;
+}
+
+async function handlePut(splittedRoute, headers, data, query) {
+    var res = { statusCode: 302, location: '/404' };
+    
+    if(splittedRoute.length == 0) {
+        res = {
+            statusCode: 200,
+            contentType: 'application/json',
+            content: JSON.stringify(await attachClient(headers, data, query))
+        }
+    }
+
+    return res;
+}
+
+async function attachClient(headers, data, query) {
+    var res = {"success": false};
+    if(headers["authorization"] && data["client_code"]) {
+        var token = headers["authorization"].replace("Bearer ", "");
+        var clientCode = data["client_code"];
+        var clientCodeId = clientCode.slice(2).slice(0, -2);
+
+        var userAndClientRes = await query(`
+            SELECT U.id as user_id, C.name as client_name, C.firstname as client_firstname, C.birthdate as client_birthdate \
+            FROM user U \
+            JOIN user_token UT \
+            ON U.id = UT.user \
+            LEFT OUTER JOIN client C \
+            ON C.id=${clientCodeId} \
+            WHERE UT.token="${token}"
+        `);
+
+        if(userAndClientRes.length == 1 && userAndClientRes[0]["client_name"]) { // s'il y a bien un utilisateur avec ce token et un client avec cet ID
+            var userAndClient = userAndClientRes[0];
+            var expectedCode = userAndClient["client_name"].slice(0,2) + clientCodeId + userAndClient["client_firstname"].slice(0,2);
+            if(clientCode == expectedCode) { // le code est bon
+                await query(`
+                    INSERT INTO user_client VALUES (${clientCodeId}, ${userAndClient["user_id"]})
+                `);
+                res["success"] = true;
+                res["client"] = {
+                    "name": userAndClient["client_name"],
+                    "firstname": userAndClient["client_firstname"],
+                    "birthdate": userAndClient["client_birthdate"]
+                }
+            } else { res["reason"] = "Code incorrect"; }
+        } else { res["reason"] = "Informations invalides"; }
     }
 
     return res;
